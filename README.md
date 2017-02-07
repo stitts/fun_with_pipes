ccef25bda4466f2c0d6f6f875043cac44dd7c557
Thomas Stitt <stitt4@llnl.gov>
2017-02-07 14:11:24 -0800

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+Redirecting stdout to a buffer:
+    
+    > g++ fun_with_pipes.cpp
+    > ./a.out
+    1: writing to stdout
+    2: printing 'wow some text' to stdout
+    2: buffer contents: '1: wow some text'
+    2: closing buffered pipe
+    1: printing to stdout again
---
+++ fun_with_pipes.cpp
@@ -0,0 +1,75 @@
+//#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define PRINT_TO_STREAM(stream, ...) fprintf(stream, "%d: ", fileno(stream)); fprintf(stream, __VA_ARGS__)
+
+struct buffered_pipe {
+  int m_fd[2];
+  int m_old_fd;
+  int m_stream_fd;
+  FILE* m_stream;
+  size_t m_buffer_len;
+  char* m_buffer;
+
+  buffered_pipe(FILE* stream, size_t buffer_len = 4096)
+    : m_old_fd(-1),
+      m_stream_fd(fileno(stream)),
+      m_stream(stream),
+      m_buffer_len(buffer_len),
+      m_buffer(NULL) {
+    m_buffer = new char[buffer_len];
+  }
+
+  int init() {
+    if (pipe(m_fd) == -1) {
+      perror("problem creating pipe: ");
+      return 1;
+    }
+    m_old_fd = dup(m_stream_fd);
+    dup2(m_fd[1], m_stream_fd);
+    return 0;
+  }
+
+  const char* read() {
+    ssize_t read_len;
+    fflush(m_stream);
+    if ((read_len = ::read(m_fd[0], m_buffer, m_buffer_len)) == -1) {
+       perror("read error: ");
+       return NULL;
+    }
+    m_buffer[read_len] = '\0';
+    return m_buffer;
+  }
+
+  ~buffered_pipe() {
+    close(m_fd[0]);
+    close(m_fd[1]);
+    // restore
+    dup2(m_old_fd, m_stream_fd);
+  }
+};
+
+
+int main() {
+  PRINT_TO_STREAM(stdout, "writing to stdout\n");
+
+  buffered_pipe* b = new buffered_pipe(stdout);
+  if (b->init()) {
+    fprintf(stderr, "problem creating buffered_pipe\n");
+    delete b;
+    return 1;
+  }
+
+  const char* text = "wow some text";
+  PRINT_TO_STREAM(stderr, "printing '%s' to stdout\n", text);
+  PRINT_TO_STREAM(stdout, "%s", text);
+  PRINT_TO_STREAM(stderr, "buffer contents: '%s'\n", b->read());
+
+  PRINT_TO_STREAM(stderr, "closing buffered pipe\n");
+  delete b;
+  PRINT_TO_STREAM(stdout, "printing to stdout again\n");
+
+  return 0;
+}
